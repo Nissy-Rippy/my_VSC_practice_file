dd05cfdd06d37c37fab3dc17981613f966ed70b2
Toyoda Yoshiteru <6089more@gmail.com>
2021-11-21 23:39:38 +0900

    [復習]

--- test/javascript.js
+++ test/javascript.js
@@ -86,10 +86,73 @@ return result;
 }
 
 var result1 = add(1, 1); 
-console.log(result1); //2
+console.log(result1);
 
-var result2 = add(10, 20); 
-console.log(result2); //30
+var result2 = add(10, 20);
+console.log(result2);
 
 var result3 = add(result1, result2);
-console.log(result3); //32
+console.log(result3);
+
+
+  let index = 0;
+  $('.main-img').eq(index).addClass('current-img');
+  setInterval(function () {
+    $('.main-img').eq(index).removeClass('current-img');
+    if (index == $('.main-img').length - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    $('.main-img').eq(index).addClass('current-img');
+  }, 3000);
+});
+
+// ポートフォリオの復習javascript
+
+(document).on('turbolinks:load', function(){
+    const inputForm = $('#searching-form');
+    const searchResult = $('.result ul');
+  function builtHTML(data){
+    let html = `
+    <li><a href="/videos/${data.id}/edit" data-method="get">${data.title}<a></li>
+    `
+    searchResult.append(html);
+  }
+
+  function NoResult(message){
+    let html = `
+    <li>${message}</li>
+    `
+    searchResult.append(html);
+
+  }
+  
+  inputForm.on('keyup', function(){
+    const target = $(this).val();
+    search(target);
+  });
+
+  function search(target){
+    $.ajax({
+      type: 'GET',
+      url: '/videos/search',
+      data: { key_word: target },
+      dataType: 'json'
+    })
+    .done(function(data){
+      searchResult.empty();
+
+      if (data.length !== 0){
+        data.forEach(function(data){
+          builtHTML(data)
+        });
+      }else{
+        NoResult('検索結果がありませんでした')
+      }
+    })
+    .fail(function(data){
+      alert("そもそも、非同期通信が行えてないよ！");
+    })
+  }
+});
