be2dae11e8a6df12dbbdc1a54749121ded28809e
Toyoda Yoshiteru <6089more@gmail.com>
2021-11-29 23:44:47 +0900

    [finish]確認問題

--- Ready01.java
+++ Ready01.java
@@ -286,3 +286,44 @@ public class Interface01 {
         camera.takePicture();
     }
 }
+
+package practice;
+
+import extend.phone.MobilePhone;
+import extend.phone.Phone;
+
+public class Chapter13 {
+    public static void main(String[] args) {
+        MobilePhone mobilePhone = new MobilePhone("000-1111-2222");
+        mobilePhone.takePicture();
+        Chapter13.staticCall(mobilePhone, "99-8888-7777");
+        mobilePhone.powerOff();
+
+        FlyingPhone flyingPhone = new FlyingPhone(5);
+        flyingPhone.fly();
+        Chapter13.staticCall(flyingPhone, "000-1111-2222");
+        flyingPhone.powerOff();
+    }
+
+    public static void staticCall(Phone phone, String number) {
+        phone.call(number);
+    }
+}
+
+package practice;
+
+public interface Flying {
+
+    void fly();
+
+    default void powerOff() {
+        System.out.println("飛んでいる場合は落下します。");
+    }
+}
+
+package practice;
+
+import extend.phone.Phone;
+
+public class FlyingPhone implements Phone, Flying {
+}
