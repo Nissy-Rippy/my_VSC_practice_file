5ae37fcab6ee59c6f2a7f4671ea369ae9fb22c58
Toyoda Yoshiteru <6089more@gmail.com>
2021-12-11 23:54:09 +0900

    [add]復習

--- lib/effects.rb
+++ lib/effects.rb
@@ -16,3 +16,20 @@ module Effects
         end
     end
 end
+
+class WordSynth
+    def initialize
+        @effects = []
+    end
+    
+    def add_effect(effect)
+        @effects << effect
+    end
+
+    def play(original_words)
+        @effects.inject(original_words) do |words,synth|
+          synth.call(words)
+        end
+    end 
+
+end
