41cd3cdc3daf48aa2b3ba5ee2efcc1b173cd2c39
Toyoda Yoshiteru <6089more@gmail.com>
2021-12-28 23:35:19 +0900

    [java]

--- java.java
+++ java.java
@@ -1,7 +1,9 @@
 public class java {
   public static void main(String[] args) {
-
+    public String name;
+    System.out.println("どうもー！");
     System.out.println("こんにちはーーー");
+    System.out.println("オッケー牧場！！！！");
   }
 
 }
