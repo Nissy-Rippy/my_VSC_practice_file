878a8f89c7d92e8d0201a0c04ebbec0ac42f4f39
Toyoda Yoshiteru <6089more@gmail.com>
2021-12-30 10:54:14 +0900

    [java修正]

--- classMethod/ClassMethod02.java
+++ classMethod/ClassMethod02.java
@@ -8,7 +8,7 @@ public class ClassMethod02 {
     Human02 yamada = new Human02();
     System.out.println("私の名前は" + yamada.name + "です.年は" +  yamada.age + "です");
 
-    Human02 sate = new Human02("佐藤", 33);
-    System.out.println("私の名前は" + sate.name + "です" + "年は" +  sate.age + "です");
+    Human02 sate = new Human02("佐藤", 33,180);
+    System.out.println("私の名前は" + sate.name + "です" + "年は" +  sate.age + "です。そして慎重派" + sate.height);
   }
 }
--- java.java
+++ java.java
@@ -1,6 +1,6 @@
 public class java {
   public static void main(String[] args) {
-    public String name;
+    String name;
     System.out.println("どうもー！");
     System.out.println("こんにちはーーー");
     System.out.println("オッケー牧場！！！！");
